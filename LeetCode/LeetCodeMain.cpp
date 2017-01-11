6efec6d4672373d8af7264e92f17dd878a5631a4
win10_lap <liyp0095@163.com>
2017-01-11 22:04:21 +0800

    search rotated

--- LeetCode/LeetCodeMain.cpp
+++ LeetCode/LeetCodeMain.cpp
@@ -2,7 +2,8 @@
 #include <algorithm>
 
 //#include "removeDuplicates.h"
-#include "removeDuplicates2.h"
+//#include "removeDuplicates2.h"
+#include "searchRotatedArray.h"
 
 using namespace std;
 
@@ -10,9 +11,14 @@ using namespace std;
 int main()
 {
 	// remove Duplicates 
-	Solution s;
+	/*Solution s;
 	int a[10] = { 1, 1, 1, 3, 5, 6, 8, 10, 10, 10 };
 	cout << s.removeDuplicates2(a, 10) << endl;
+*/
+	// Search Rotate Array
+	Solution s;
+	int a[10] = {5, 6, 8, 10, 10, 10, 5, 5, 5, 5};
+	cout << s.search2(a, 10, 5) << endl;
 
 	system("pause");
 	return 0;
